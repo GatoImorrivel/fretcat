03d2121cb30dcd589812f72c79cf8f097496c6fc
GatoImorrivel <guilhermevianagui23@gmail.com>
2023-03-08 14:01:41 -0300

    feat: making a fuzz effect

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,11 @@ struct FretCat {
 
 #[derive(Params)]
 struct FretCatParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
     #[id = "gain"]
     pub gain: FloatParam,
+
+    #[id = "threshold"]
+    pub threshold: FloatParam,
 }
 
 impl Default for FretCat {
@@ -26,29 +25,28 @@ impl Default for FretCat {
 impl Default for FretCatParams {
     fn default() -> Self {
         Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
             gain: FloatParam::new(
                 "Gain",
                 util::db_to_gain(0.0),
                 FloatRange::Skewed {
                     min: util::db_to_gain(-30.0),
                     max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
                     factor: FloatRange::gain_skew_factor(-30.0, 30.0),
                 },
             )
-            // Because the gain parameter is stored as linear gain instead of storing the value as
-            // decibels, we need logarithmic smoothing
             .with_smoother(SmoothingStyle::Logarithmic(50.0))
             .with_unit(" dB")
-            // There are many predefined formatters we can use here. If the gain was stored as
-            // decibels instead of as a linear gain value, we could have also used the
-            // `.with_step_size(0.1)` function to get internal rounding.
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+
+            threshold: FloatParam::new(
+                "Threshold",
+                util::db_to_gain(0.0),
+                FloatRange::Linear  {
+                    min: util::db_to_gain(0.0),
+                    max: util::db_to_gain(1.0),
+                },
+            )
         }
     }
 }
@@ -103,7 +101,7 @@ impl Plugin for FretCat {
     ) -> bool {
         // Resize buffers and perform other potentially expensive initialization operations here.
         // The `reset()` function is always called right after this function. You can remove this
-        // function if you do not need it.
+        // function if you do not need it
         true
     }
 
@@ -121,9 +119,11 @@ impl Plugin for FretCat {
         for channel_samples in buffer.iter_samples() {
             // Smoothing is optionally built into the parameters themselves
             let gain = self.params.gain.smoothed.next();
+            let threshold = self.params.gain.smoothed.next();
+            nih_log!("{}", channel_samples.len());
 
             for sample in channel_samples {
-                *sample *= gain;
+                *sample = gain * libm::tanhf(*sample / gain / threshold);
             }
         }
 
