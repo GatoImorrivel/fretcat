b757dfd32a619e86d2f934c5572bfe88c6f3edea
GatoImorrivel <guilhermevianagui23@gmail.com>
2023-06-26 20:44:45 -0300

    working multi-effect view chain

--- crates/fretcat_tests/src/lib.rs
+++ crates/fretcat_tests/src/lib.rs
@@ -1,12 +1,6 @@
+#[allow(dead_code)]
+
 #[cfg(test)]
 mod tests {
-    use fretcat_derive::Control;
 
-    #[test]
-    fn control_macro() {
-        #[derive(Control)]
-        struct TestStruct {}
-
-        TestStructControl::print();
-    }
 }
