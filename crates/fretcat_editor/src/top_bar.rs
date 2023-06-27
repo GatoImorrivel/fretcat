ecd6db0299716f4aa1dbd18d42af65d1cae50abe
GatoImorrivel <guilhermevianagui23@gmail.co>
2023-06-27 16:49:03 -0300

    sync

--- crates/fretcat_editor/src/top_bar.rs
+++ crates/fretcat_editor/src/top_bar.rs
@@ -1,6 +1,18 @@
-use nih_plug_vizia::vizia::{image, prelude::*};
+use nih_plug_vizia::vizia::{
+    image::{self, ImageFormat},
+    prelude::*,
+};
 
 pub fn top_bar(cx: &mut Context) {
-    HStack::new(cx, |cx| {
-    }).class("top-bar");
+    cx.load_image(
+        "logo.png",
+        image::load_from_memory_with_format(include_bytes!("res/logo.png"), ImageFormat::Png)
+            .unwrap(),
+        ImageRetentionPolicy::Forever,
+    );
+
+    cx.add_stylesheet(include_style!("../css/topbar.css"))
+        .unwrap();
+
+    Element::new(cx).class("logo");
 }
