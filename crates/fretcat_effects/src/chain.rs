50deb6b7d129d73d44d379f0936d17e2d8c7f3cf
GatoImorrivel <guilhermevianagui23@gmail.com>
2023-08-03 23:38:47 -0300

    Revert "save"
    This reverts commit 19bb7f18f0a801bc94f9c5599b3e4b26360f0f9b.

--- crates/fretcat_effects/src/chain.rs
+++ crates/fretcat_effects/src/chain.rs
@@ -1,87 +1,74 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, hash::Hash, any::TypeId};
 
-use crossbeam::queue::ArrayQueue;
+use crate::{effect::{Effect, AudioEffect}, overdrive::Overdrive};
 
-use crate::{
-    effect::{AudioEffect, Effect},
-    overdrive::Overdrive,
-};
-
-pub type Query<'a> = &'a Box<dyn AudioEffect>;
-pub type QueryMut<'a> = &'a mut Box<dyn AudioEffect>;
-
-#[derive(Debug, Clone)]
-pub enum ChainCommand {
-    Insert(Box<dyn AudioEffect>)
-}
+pub type Query<'a> = (TypeId, &'a Box<dyn AudioEffect>);
+pub type QueryMut<'a> = (TypeId, &'a mut Box<dyn AudioEffect>);
 
 #[derive(Debug, Clone)]
 pub struct Chain {
     pub effects: Vec<Effect>,
-    pub update_queue: Arc<ArrayQueue<ChainCommand>>,
     data_cache: HashMap<Effect, Box<dyn AudioEffect>>,
+    type_cache: HashMap<Effect, TypeId>
 }
 
 impl Chain {
-    pub fn add_to_queue(&self, command: ChainCommand) {
-        self.update_queue.force_push(command);
-    }
-
-    pub fn handle_command(&mut self, command: ChainCommand) {
-        match command {
-            ChainCommand::Insert(data) => {
-                self.insert(data);
-            }
-        }
-    }
-
-    pub fn insert(&mut self, audio_effect: Box<dyn AudioEffect>) -> (usize, Effect) {
+    pub fn insert(&mut self, audio_effect: impl AudioEffect) -> (usize, Effect) {
+        let audio_effect = Box::new(audio_effect);
+        let type_id = audio_effect.type_id();
         let e = Effect::new();
         self.effects.push(e);
         self.data_cache.insert(e, audio_effect);
+        self.type_cache.insert(e, type_id);
         self.effects.clone().into_iter().enumerate().last().unwrap()
     }
 
     pub fn insert_at(&mut self, index: usize, audio_effect: Box<dyn AudioEffect>) -> Effect {
         let e = Effect::new();
         self.effects.insert(index, e);
+        self.type_cache.insert(e, audio_effect.type_id());
         self.data_cache.insert(e, audio_effect);
         self.effects.clone()[index]
     }
 
     pub fn remove(&mut self, effect: &Effect) {
-        let fetch = self
-            .effects
-            .clone()
-            .into_iter()
-            .enumerate()
-            .find(|(_i, e)| e == effect);
+        let fetch = self.effects.clone().into_iter().enumerate().find(|(_i, e)| {
+            e == effect
+        });
 
         if let Some((index, effect)) = fetch {
             self.effects.remove(index);
             self.data_cache.remove(&effect);
+            self.type_cache.remove(&effect);
         }
     }
 
+    pub fn get(&self, effect: &Effect) -> Option<&Box<dyn AudioEffect>> { 
+        self.data_cache.get(effect)
+    }
+
+    pub fn get_mut(&mut self, effect: &Effect) -> Option<&mut Box<dyn AudioEffect>> { 
+        self.data_cache.get_mut(effect)
+    }
+
     pub fn query(&self, effect: &Effect) -> Option<Query> {
-        let c = self.data_cache.get(&effect)?;
-        Some(c)
+        let c = self.get(&effect)?;
+
+        Some((self.type_cache.get(effect)?.clone(), c))
     }
 
     pub fn query_mut(&mut self, effect: &Effect) -> Option<QueryMut> {
-        let c = self.data_cache.get_mut(effect)?;
-        Some(c)
+        let t = self.type_cache.get(effect)?.clone();
+        let c = self.get_mut(effect)?;
+        Some((t, c))
     }
 
     pub fn query_cast<T: AudioEffect + 'static>(&self, effect: &Effect) -> Option<&T> {
-        self.data_cache.get(effect)?.as_any().downcast_ref::<T>()
+        self.get(effect)?.as_any().downcast_ref::<T>()
     }
 
     pub fn query_cast_mut<T: AudioEffect + 'static>(&mut self, effect: &Effect) -> Option<&mut T> {
-        self.data_cache
-            .get_mut(effect)?
-            .as_any_mut()
-            .downcast_mut::<T>()
+        self.get_mut(effect)?.as_any_mut().downcast_mut::<T>()
     }
 
     pub fn query_index(&self, index: usize) -> Option<(Effect, &Box<dyn AudioEffect>)> {
@@ -93,6 +80,7 @@ impl Chain {
         let data = self.data_cache.get(e).unwrap();
         Some((*e, data))
     }
+
 }
 
 impl Default for Chain {
@@ -100,10 +88,10 @@ impl Default for Chain {
         let mut chain = Chain {
             effects: vec![],
             data_cache: HashMap::new(),
-            update_queue: Arc::new(ArrayQueue::new(5).into())
+            type_cache: HashMap::new()
         };
 
-        chain.insert(Box::new(Overdrive::default()));
+        chain.insert(Overdrive::default());
 
         chain
     }
--- crates/fretcat_effects/src/lib.rs
+++ crates/fretcat_effects/src/lib.rs
@@ -1,3 +1,5 @@
+use strum::{EnumIter, IntoEnumIterator};
+
 mod chain;
 mod effect;
 
@@ -8,6 +10,30 @@ pub use atomic_refcell::*;
 
 pub use fuzz::Fuzz;
 pub use overdrive::Overdrive;
-pub use chain::{Chain, ChainCommand};
+pub use chain::Chain;
 pub use effect::{Effect, AudioEffect};
 pub use dyn_clone;
+
+#[derive(Debug, EnumIter, Clone, Copy, PartialEq, Hash, Eq)]
+pub enum EffectKind {
+    Distortion,
+    Delay,
+    Dynamics,
+    Reverb,
+    Echo,
+}
+
+impl EffectKind {
+    pub fn variants() -> Vec<Self> {
+        EffectKind::iter().fold(vec![], |mut acc, kind| {
+            acc.push(kind);
+            acc
+        })
+    }
+}
+
+impl ToString for EffectKind {
+    fn to_string(&self) -> String {
+        format!("{:?}", self)
+    }
+}
