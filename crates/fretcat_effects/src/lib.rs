19bb7f18f0a801bc94f9c5599b3e4b26360f0f9b
GatoImorrivel <guilhermevianagui23@gmail.com>
2023-08-03 23:25:51 -0300

    save

--- crates/fretcat_effects/src/lib.rs
+++ crates/fretcat_effects/src/lib.rs
@@ -1,5 +1,3 @@
-use strum::{EnumIter, IntoEnumIterator};
-
 mod chain;
 mod effect;
 
@@ -10,30 +8,6 @@ pub use atomic_refcell::*;
 
 pub use fuzz::Fuzz;
 pub use overdrive::Overdrive;
-pub use chain::Chain;
+pub use chain::{Chain, ChainCommand};
 pub use effect::{Effect, AudioEffect};
 pub use dyn_clone;
-
-#[derive(Debug, EnumIter, Clone, Copy, PartialEq, Hash, Eq)]
-pub enum EffectKind {
-    Distortion,
-    Delay,
-    Dynamics,
-    Reverb,
-    Echo,
-}
-
-impl EffectKind {
-    pub fn variants() -> Vec<Self> {
-        EffectKind::iter().fold(vec![], |mut acc, kind| {
-            acc.push(kind);
-            acc
-        })
-    }
-}
-
-impl ToString for EffectKind {
-    fn to_string(&self) -> String {
-        format!("{:?}", self)
-    }
-}
