a5ca0d4110646893e814cb03de60f97255a6bb1d
GatoImorrivel <guilhermevianagui23@gmail.com>
2023-10-12 14:53:27 -0300

    fix textbox

--- crates/fretcat_editor/src/components/preset_control.rs
+++ crates/fretcat_editor/src/components/preset_control.rs
@@ -4,24 +4,41 @@ pub use nih_plug::vizia::prelude::*;
 
 #[derive(Debug, Clone, Lens)]
 pub struct PresetControl {
-    pub preset_name: String
+    pub preset_name: String,
+    pub color: Color,
 }
 
 pub enum PresetMessage {
     Save,
-    TextChange(String)
+    TextChange(String),
+    ChangeColor(Color),
 }
 
 impl PresetControl {
     pub fn new(cx: &mut Context) {
-        Self { preset_name: "Untitled".to_owned() }.build(cx, |cx| {
+        Self {
+            preset_name: "Untitled".to_owned(),
+            color: Color::transparent(),
+        }
+        .build(cx, |cx| {
             HStack::new(cx, |cx| {
                 HStack::new(cx, |cx| {
-                    Textbox::new_multiline(cx, Self::preset_name, true).class("preset-name")
-                    .on_edit(|cx, str| {
-                        cx.emit(PresetMessage::TextChange(str));
-                    });
-                }).class("name-wrapper");
+                    Textbox::new_multiline(cx, Self::preset_name, true)
+                        .class("preset-name")
+                        .on_edit(|cx, str| {
+                            cx.emit(PresetMessage::TextChange(str));
+                        })
+                        .on_submit(|ex, _, _| {
+                            ex.emit(PresetMessage::ChangeColor(Color::transparent()));
+                        })
+                        .on_press_down(|ex| {
+                            ex.emit(PresetMessage::ChangeColor(Color::whitesmoke()));
+                        })
+                        .bind(Self::color, |me, bind| {
+                            me.caret_color(bind.0);
+                        });
+                })
+                .class("name-wrapper");
                 Button::new(
                     cx,
                     |ex| ex.emit(PresetMessage::Save),
@@ -48,7 +65,12 @@ impl View for PresetControl {
                 preset.save();
             }
             PresetMessage::TextChange(text) => {
-                self.preset_name = text.to_owned();
+                if text.len() > 0 {
+                    self.preset_name = text.to_owned();
+                }
+            }
+            PresetMessage::ChangeColor(color) => {
+                self.color = *color;
             }
         });
     }
